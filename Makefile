6696ed1e8385d47a5ca36d60188b6cc2e58280b9
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-09 07:32:51 -0700

    switch lab 1 and lab 3

---
+++ Makefile
@@ -0,0 +1,11 @@
+##
+# Project Title
+#
+# @file
+# @version 0.1
+
+all:
+	make --directory ./lab01/
+	make --directory ./lab03/
+
+# end
---
+++ lab03/src/main.cpp
@@ -0,0 +1,74 @@
+// CS 211 Fall 2024 – Week 03 Lab
+// Adam Neeley
+//---------------------------------------------------------------
+// File: main.cpp
+
+// #include "Bag.h"
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+  // declarations
+  // Bag bag1;
+  // Bag bag2(100);
+
+  cout << boolalpha;
+  cout << endl;
+  cout << "Lab 3" << endl;
+  cout << "Adam Neeley" << endl;
+  cout << endl;
+
+  // // verify the constructors and test the accessors/observers
+  // //        at the same time
+  // cout << "VERIFYING CONSTRUCTORS/ACCESSORS/OBSERVERS:" << endl;
+  // cout << endl;
+  // cout << "true means test passed, false means test failed:" << endl;
+  // cout << "-------------------------------------------" << endl;
+
+  // cout << (bag1.get_size() == 0) << endl;
+  // cout << (bag2.get_size() == 0) << endl;
+
+  // cout << (bag1.get_count(3) == 0) << endl;
+
+  // cout << (bag1.get_capacity() == 30) << endl;
+  // cout << (bag2.get_capacity() == 100) << endl;
+
+  // // verifying the modifiers
+
+  // cout << endl;
+  // cout << "VERIFYING MODIFIERS:" << endl;
+  // cout << endl;
+  // cout << "true means test passed, false means test failed:" << endl;
+  // cout << "-------------------------------------------" << endl;
+
+  // bag1.insert(3);
+  // bag1.insert(3);
+  // bag1.insert(4);
+  // bag1.insert(4);
+  // bag1.insert(4);
+
+  // cout << (bag1.get_size() == 5) << endl;
+  // cout << (bag1.get_count(3) == 2) << endl;
+  // cout << (bag1.get_count(4) == 3) << endl;
+
+  // bag1.erase(4);
+  // bag1.erase_one(3);
+
+  // cout << (bag1.get_size() == 1) << endl;
+  // cout << (bag1.get_count(3) == 1) << endl;
+  // cout << (bag1.get_count(4) == 0) << endl;
+
+  // bag1.reserve(1000);
+
+  // cout << (bag1.get_capacity() == 1000) << endl;
+  // cout << (bag1.get_size() == 1) << endl;
+  // cout << (bag1.get_count(3) == 1) << endl;
+  // cout << (bag1.get_count(4) == 0) << endl;
+
+  // other methods/operators tests -- can use same basic approach as
+  //        for functions, except they can be included in this same
+  //        testing function.
+
+  return EXIT_SUCCESS;
+}
