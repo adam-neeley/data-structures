c8a7134c7340c8625e0f399cf7b88a89f019ac0b
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-09 07:54:27 -0700

    clean

--- lab03/src/main.cpp
+++ lab03/src/main.cpp
@@ -9,66 +9,14 @@
 using namespace std;
 
 int main() {
-  // declarations
-  // Bag bag1;
-  // Bag bag2(100);
+
+  string lines[] = {"Lab 3", "Adam", "9/9/24"};
 
   cout << boolalpha;
   cout << endl;
-  cout << "Lab 3" << endl;
-  cout << "Adam Neeley" << endl;
+  for (const string line : lines)
+    cout << line << endl;
   cout << endl;
-
-  // // verify the constructors and test the accessors/observers
-  // //        at the same time
-  // cout << "VERIFYING CONSTRUCTORS/ACCESSORS/OBSERVERS:" << endl;
-  // cout << endl;
-  // cout << "true means test passed, false means test failed:" << endl;
-  // cout << "-------------------------------------------" << endl;
-
-  // cout << (bag1.get_size() == 0) << endl;
-  // cout << (bag2.get_size() == 0) << endl;
-
-  // cout << (bag1.get_count(3) == 0) << endl;
-
-  // cout << (bag1.get_capacity() == 30) << endl;
-  // cout << (bag2.get_capacity() == 100) << endl;
-
-  // // verifying the modifiers
-
-  // cout << endl;
-  // cout << "VERIFYING MODIFIERS:" << endl;
-  // cout << endl;
-  // cout << "true means test passed, false means test failed:" << endl;
-  // cout << "-------------------------------------------" << endl;
-
-  // bag1.insert(3);
-  // bag1.insert(3);
-  // bag1.insert(4);
-  // bag1.insert(4);
-  // bag1.insert(4);
-
-  // cout << (bag1.get_size() == 5) << endl;
-  // cout << (bag1.get_count(3) == 2) << endl;
-  // cout << (bag1.get_count(4) == 3) << endl;
-
-  // bag1.erase(4);
-  // bag1.erase_one(3);
-
-  // cout << (bag1.get_size() == 1) << endl;
-  // cout << (bag1.get_count(3) == 1) << endl;
-  // cout << (bag1.get_count(4) == 0) << endl;
-
-  // bag1.reserve(1000);
-
-  // cout << (bag1.get_capacity() == 1000) << endl;
-  // cout << (bag1.get_size() == 1) << endl;
-  // cout << (bag1.get_count(3) == 1) << endl;
-  // cout << (bag1.get_count(4) == 0) << endl;
-
-  // other methods/operators tests -- can use same basic approach as
-  //        for functions, except they can be included in this same
-  //        testing function.
 
   return EXIT_SUCCESS;
 }
