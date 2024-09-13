da894229ee402661580eab9ca1966a900339a945
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-12 17:11:50 -0700

    zylab

--- zylab/02/Searcher.h
+++ zylab/02/Searcher.h
@@ -11,46 +11,25 @@ using namespace std;
 
 template <typename T> class Searcher {
 public:
-  // Returns the index of the key in the sorted array, or -1 if the key is not
-  // found
   static int BinarySearch(T *array, int arraySize, const T &key,
                           Comparer<T> &comparer) {
-
-    if (arraySize == 0)
-      return -1;
-
-    int left = 0;
-    int right = arraySize - 1;
-    int mid;
-
-    if (arraySize % 2 == 0)
-      mid = arraySize / 2;
-    else
-      mid = (arraySize - 1) / 2;
-
-    cout << "mid: " << mid << endl;
-
-    switch (comparer.Compare(key, array[mid])) {
-    case 1:
-      return BinarySearch(sliceArray(array, mid, right), right - mid, key,
-                          comparer);
-    case -1:
-      return BinarySearch(sliceArray(array, left, mid), mid - left, key,
-                          comparer);
-    default:
-    case 0:
-      return mid;
-    }
+    return BinarySearchRec(array, 0, arraySize - 1, key, comparer);
   }
 
 private:
-  static T *sliceArray(T *array, int start, int end) {
-    T *res = new T[end - start + 1];
-    for (int i = 0; i <= end - start; i++) {
-      res[i] = array[start + i];
+  static int BinarySearchRec(T *array, int left, int right, const T &key,
+                             Comparer<T> &comparer) {
+    if (right >= left) {
+      const int len = right - left;
+      const int mid = left + len / 2;
+      if (comparer.Compare(array[mid], key) < 0)
+        return BinarySearchRec(array, mid + 1, right, key, comparer);
+      if (comparer.Compare(array[mid], key) > 0)
+        return BinarySearchRec(array, left, mid - 1, key, comparer);
+      return mid;
     }
-    return res;
+
+    return -1;
   }
 };
-
 #endif
