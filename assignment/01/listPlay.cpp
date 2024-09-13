f44edd5bf94b9256b63ee92135ea4210c4806e15
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-12 18:29:29 -0700

    assign 1 (recursive)

--- assignment/01/listPlay.cpp
+++ assignment/01/listPlay.cpp
@@ -1,5 +1,6 @@
 // CS 211 Fall 2024 - Programming Assignment #1
-//
+// Modified by: Adam Neeley
+// 9/12/24
 
 #include <cstdlib>
 #include <iostream>
@@ -9,60 +10,59 @@
 
 using namespace std;
 
+Node *inputList() {
+  double value;
+
+  cout << "Enter a numeric value (-1 to finish): ";
+  cin >> value;
+
+  if (value == -1)
+    return NULL;
+
+  Node *node;
+  node = new Node(value);
+  node->setLink(inputList());
+  return node;
+}
+
+void printList(Node *node) {
+  if (node == NULL)
+    return;
+  cout << " " << node->getData();
+  printList(node->getLink());
+}
+
+double sumList(Node *node) {
+  if (node == NULL)
+    return 0;
+  return node->getData() + sumList(node->getLink());
+}
+
+double productList(Node *node) {
+  if (node == NULL)
+    return 1;
+  return node->getData() * productList(node->getLink());
+}
+
+void deleteList(Node *node) {
+  if (node == NULL)
+    return;
+  deleteList(node->getLink());
+  delete node;
+}
+
 void listPlay() {
+  Node *head = inputList();
 
-  Node *head = NULL;
-  Node *nodePtr = head;
-
-  int value;
-
-  while (true) {
-    std::cout << "Enter a numeric value (-1 to finish): ";
-    std::cin >> value;
-
-    if (value == -1)
-      break; // -1 escapes loop
-
-    Node *nextNodePtr;
-    nextNodePtr = new Node(value); // create next node
-
-    if (head == NULL)
-      head = nextNodePtr;
-    else
-      nodePtr->setLink(nextNodePtr);
-
-    nodePtr = nextNodePtr;
-  }
-
-  //
-
-  std::cout << "Numbers entered:";
-
-  double sum = 0;
-  double product = 1;
-
-  nodePtr = head;
-  while (nodePtr != NULL) {
-    double data = nodePtr->getData();
-    sum += data;
-    product *= data;
-    std::cout << " " << nodePtr->getData();
-    nodePtr = nodePtr->getLink();
-  }
+  cout << "Numbers entered:";
+  printList(head);
+  cout << endl;
 
   if (head == NULL)
     return;
 
-  std::cout << std::endl;
-  std::cout << "Sum: " << sum << std::endl;
-  std::cout << "Product: " << product << std::endl;
+  cout << "Sum: " << sumList(head) << endl;
+  cout << "Product: " << productList(head) << endl;
 
-  // delete list
-  nodePtr = head;
-  while (nodePtr != NULL) {
-    Node *nextPtr = nodePtr->getLink();
-    std::cout << "Deleting: " << nodePtr->getData() << std::endl;
-    delete nodePtr;
-    nodePtr = nextPtr;
-  }
+  deleteList(head);
 };
--- assignment/01/listPlay.h
+++ assignment/01/listPlay.h
@@ -1,9 +1,17 @@
 // CS 211 Fall 2024 - Programming Assignment #1
-//
+// Modified by: Adam Neeley
+// 9/12/24
 
 #ifndef LISTPLAY_H
 #define LISTPLAY_H
 
+#include "Node.h"
+
+Node *inputList();
+double productList(Node *node);
+double sumList(Node *node);
+void deleteList(Node *node);
 void listPlay();
+void printList(Node *node);
 
 #endif
