dada01d7435358352138be0946cd099fbaed8645
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-16 11:02:19 -0700

    start lab 4

---
+++ lab/04/files/week04Lab.cpp
@@ -0,0 +1,45 @@
+// CS 211 Fall 2024 - Week 04 Lab
+//
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// euclideanGCD: unsigned long long int, unsigned long long int
+//               --> unsigned long long int
+// Expects two integers from 1 to 18,446,744,073,709,551,615 (2^64 - 1)
+// Returns the grestest common factor of the two values
+//     employing Tail Recursion
+
+unsigned long long int euclideanGCD(unsigned long long int a,
+                                    unsigned long long int b) {
+    return 0;  // REPLACE WITH YOUR CODE
+}
+
+// geometricSum: unsigned int -> double
+// Expects a non-negative integer (we'll call it n)
+// Returns the sum of terms 1 + 1/2 + 1/4 + ... + 1/(2^n)
+//     employing Tail Recursion
+//     HINT:  Consider using two functions, one that is called
+//            by main(), and a "helper" function called by
+//            geometricSum to perform the Tail Recursion
+// Remember, the recurrence relation is f(n) = 1 + (0.5 * f(n-1))
+
+double geometricSum(unsigned int n) {
+    return 0;  // REPLACE WITH YOUR CODE
+}
+
+// smallestElement: double[], int -> double
+// Epxects an array of values, and the array's size
+// Returns the smallest (least) value of the elements in the array
+//     employing Tail Recursion
+//     HINT:  Consider using two functions, one that is called
+//            by main(), and a "helper" function called by
+//            smallestElement to perform the Tail Recursion
+
+double smallestElement(double anArray[], int size) {
+    return 0;  // REPLACE WITH YOUR CODE
+}
+
