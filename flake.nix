a37c056d19dd2005bf80b3b282cad7475283f251
adam-neeley <adam.t.neeley@protonmail.com>
2024-09-17 22:11:51 -0700

    setup flake packages for labs
    plus lab 5

--- flake.nix
+++ flake.nix
@@ -3,30 +3,50 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-24.05";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, }:
     let
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
+      supportedSystems = [
+        "x86_64-linux"
+        # "aarch64-linux"
+        # "x86_64-darwin"
+        # "aarch64-darwin"
+      ];
+      dirs = [ ./lab ./zylab ./assignment ];
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      lib = import ./lib pkgs;
+      labs = pkgs.lib.attrsets.mapAttrsToList (k: v: k) (lib.getDir ./lab);
     in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs;
-            [
-              clang-tools
-              cmake
-              codespell
-              conan
-              cppcheck
-              doxygen
-              gtest
-              lcov
-              vcpkg
-              vcpkg-tool
-            ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+      packages.${system} = pkgs.lib.attrsets.mergeAttrsList (map (lab: {
+        "lab-${lab}" = pkgs.stdenv.mkDerivation rec {
+          name = "lab-${lab}";
+          src = ./lab/${lab};
+          installPhase = ''
+            mkdir -p $out/bin
+            cp Lab $out/bin/${name}
+          '';
+          meta.mainProgram = name;
         };
-      });
+      }) labs);
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs;
+          [
+            (writeShellScriptBin "nix-build-package" ''
+              REALPATH=realpath $1
+              nix-build -E 'with import <nixpkgs> {}; callPackage "$REALPATH" {}'
+            '')
+            clang-tools
+            cmake
+            codespell
+            conan
+            cppcheck
+            doxygen
+            gtest
+            lcov
+            vcpkg
+            vcpkg-tool
+          ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+      };
     };
 }
---
+++ lab/05/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+auto main(int argc, char *argv[]) -> int {
+  std::cout << "hi from main" << std::endl;
+
+  return 0;
+}
